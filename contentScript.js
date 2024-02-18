c0165f905e78bffcabfef5af91272847927fa2f3
Thomas Stephens <tmsteph1290@gmail.com>
2024-02-18 22:22:24 +0000

    Add Logging

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,29 @@
 document.addEventListener('keydown', function(event) {
+    console.log("Key pressed:", event.code); // Log the code of the key pressed
+    
     if (!event.altKey) { // Check if Alt key is pressed for example
         switch(event.keyCode) {
             case 98: // Numpad 2: Scroll down
+                console.log('Action: Scroll down');
                 window.scrollBy(0, 100);
                 break;
             case 104: // Numpad 8: Scroll up
+                console.log('Action: Scroll up');
                 window.scrollBy(0, -100);
                 break;
             case 100: // Numpad 4: Scroll left
+                console.log('Action: Scroll left');
                 window.scrollBy(-100, 0);
                 break;
             case 102: // Numpad 6: Scroll right
+                console.log('Action: Scroll right');
                 window.scrollBy(100, 0);
                 break;
+            default:
+                console.log('Action: No mapping for this key');
+                break;
         }
+    } else {
+        console.log('Alt key is pressed, ignoring numpad input.');
     }
 });
